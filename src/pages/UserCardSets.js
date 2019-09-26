c2471bfb0ca602ace43ce6eca97a764589c62210
QMaximillian <quinnlashinsky@gmail.com>
2019-09-26 17:52:59 -0400

    Updated cardSets to have a checked property for future batch updating or deleting purposes

--- src/pages/UserCardSets.js
+++ src/pages/UserCardSets.js
@@ -1,24 +1,79 @@
 import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { fetchGetCardSetIndex } from '../fetchRequests/cardSets'
+import TextBox from '../components/TextBox'
 
 export default function UserCardSets(props){
     const [cardSets, setCardSets] = useState([])
-    useEffect(() => {
-        fetchGetCardSetIndex().then(r => setCardSets(r));
-        console.log('here')
-    }, [])
+    const [search, setSearch] = useState({name: '', value: '', isValid: true})
     
+    useEffect(() => {
+        fetchGetCardSetIndex().then(r => {
+          const sanitizeResults = r.map(e => ({checked: false, ...e}))
+          return sanitizeResults
+        }).then(r => 
+            setCardSets(r))
+            
+
+    }, [])
+
+    // useEffect(() => {
+    //   console.log("cardSets", cardSets);
+    // }, [cardSets])
+    
+    // function handleChecked(cardSet, idx){
+    //   if (cardSet[idx].checked) {
+    //     cardSet.checked = false
+    //   } else {
+    //     cardSet.checked = true
+    //   }
+    // }
        return (
-           <div>
-               {cardSets.map((cardSet, idx) => {
-                   return (
-                     <Link to={`/card-sets/${cardSet.id}`}>
-                       <div key={idx}>{cardSet.name}</div>
-                     </Link>
-                   );
-               })}
+         <div className="w-full border-yellow-500 border">
+           <TextBox
+             name="search-bar"
+             type="text"
+             value={search.value}
+             onChange={setSearch}
+             placeholder={"Title"}
+           />
+           <div className="flex flex-wrap border-orange-500 border overflow-hidden justify-center">
+             {cardSets.map((cardSet, idx) => {
+               console.log(cardSet.checked)
+               return (
+                 <div  className="w-1/2 max-w-lg my-4 px-4">
+                   <div className="h-40 rounded overflow-hidden border-teal-500 border shadow-lg">
+                     <div className="mx-4 items-center border-red-500 border">
+                       <input
+                       onChange={(e) => {
+                         const updateCardSets = cardSets.map(updaterCardSet => {
+
+                           if (updaterCardSet.id === cardSets[idx].id) {
+                             return {...updaterCardSet, checked: !updaterCardSet.checked}
+                           } else {
+                             return updaterCardSet
+                           }
+                           
+                         })
+                         setCardSets(updateCardSets);
+
+                       }}
+                         className="ml-4 self-center"
+                         type="checkbox"
+                         checked={cardSet.checked}
+                       ></input>
+                       <Link to={`/card-sets/${cardSet.id}`}>
+                         <div className="text-2xl ml-20 " key={idx}>
+                           {cardSet.name}
+                         </div>
+                       </Link>
+                     </div>
+                   </div>
+                 </div>
+               );
+             })}
            </div>
-       )
+         </div>
+       );
 }
 

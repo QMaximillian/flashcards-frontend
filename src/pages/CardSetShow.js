a1d2310106f7f9850ca78cafe9c1bde02b252457
QMaximillian <quinnlashinsky@gmail.com>
2019-09-26 13:11:57 -0400

    Add ability to increment or decrement index

--- src/pages/CardSetShow.js
+++ src/pages/CardSetShow.js
@@ -1,17 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {fetchGetCardSetShow} from '../fetchRequests/cardSets'
 
 export default function CardSetShow(props){
-      
+  const [isLoading, setIsLoading] = useState(true)
+  const [flashcards, setFlashcards] = useState([])
+  const [idx, setIdx] = useState(0)
+
   useEffect(() => {
+    fetchGetCardSetShow(props.match.params.id)
+    .then(r => setFlashcards(r))
+    .then(r => setIsLoading(false))
+  }, [props.match.params.id])
 
-    fetchGetCardSetShow(props.match.params.id).then(r => console.log(r))
-  }, [props])
-
-       return (
-           <div>
-             CardSetShow
-           </div>
-       )
+      return !isLoading ? (
+        <div>
+          {flashcards[0].name}
+          <div>
+            <div>
+              {flashcards[idx].term}
+              {flashcards[idx].definition}
+            </div>
+            <div onClick={() => setIdx(idx + 1)}>
+              Next
+            </div>
+            <div onClick={() => setIdx(idx - 1)}>
+              Previous
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      );
 }
 

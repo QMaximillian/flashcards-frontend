dfb7180018cd963965a24ffde8f6340d1f115b4f
QMaximillian <quinnlashinsky@gmail.com>
2019-10-25 12:15:40 -0400

    Add hover styling for border bottom on HomeLatestCard

--- src/components/HomeLatestCard.js
+++ src/components/HomeLatestCard.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 export default function HomeLatestCard(props){
 
        return (
-         <div className="h-40 w-64 border border-black">
-           <div className="h-full w-full border border-transparent hover:border-b-8 hover:border-orange-400">
+         <div className="h-40 w-64 border border-gray-500 shadow-lg rounded">
+           <div className="h-full w-full home-latest">
              <div className="text-base font-semibold">{props.cardSetName}</div>
              <div className="opacity-50 text-sm">{props.termCount} terms</div>
              <div className="">{props.owner}</div>
-             <div className="w-full h-2 border hover:bg-orange-500"></div>
+             {/* <div className="w-full h-2 border border-black group-hover:bg-orange-500"></div> */}
            </div>
          </div>
        );

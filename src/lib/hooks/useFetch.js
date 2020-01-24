30a98446287d85360754ad4be9915ac8d4ab42f8
QMaximillian <quinnlashinsky@gmail.com>
2020-01-24 13:24:34 -0500

    Amend useFetch hook

--- src/lib/hooks/useFetch.js
+++ src/lib/hooks/useFetch.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react'
 import { BASE_HEADERS, BASE_URL } from '../../fetchRequests/baseFetchOptions'
 
-export default function useFetch(url, options = BASE_HEADERS, type = "GET"){
+export default function useFetch(url, options = {}, type = "GET"){
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
 
   useEffect(() => {
     setLoading(true)
+
+    const abortController = new AbortController()
+
     fetch(`${BASE_URL}/${url}`, {
       method: type,
+      signal: abortController.signal,
+      BASE_HEADERS,
       ...options
     })
       .then(res => {
@@ -20,6 +25,10 @@ export default function useFetch(url, options = BASE_HEADERS, type = "GET"){
         setError(error)
         setLoading(false)
       })
+
+      return function cancel() {
+        abortController.abort()
+      }
   }, [options, type, url])
 
 

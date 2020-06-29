a5bb279e5d4ef4b9aaaa0fd632cf5bf232ebe827
“Quinn <quinnlashinsky@gmail.com>
2020-06-29 19:48:14 -0400

    Setup message in case app is slow because Heroku Dyno is waking up

--- src/components/RouteDecider.js
+++ src/components/RouteDecider.js
@@ -1,16 +1,31 @@
-import React, {useContext} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Navigation from '../components/Navigation'
 import LoggedInRoutes from '../components/LoggedInRoutes'
 import LoggedOutRoutes from '../components/LoggedOutRoutes'
-import {UserContext} from '../context/user-context'
-export default function RouteDecider(props) {
-  let {user, authLoading} = useContext(UserContext)
-  if (authLoading)
-    return (
-      <div className="flex h-full w-full justify-center items-center">
-        <div className="text-6xl">Loading...</div>
-      </div>
-    )
+import { UserContext } from '../context/user-context'
+export default function RouteDecider() {
+  let { user, authLoading } = useContext(UserContext)
+  let [showMessage, setShowMessage] = useState(false)
+
+  let message = `Heroku Dyno may be waking up. If page doesn't load, refresh in 10ish seconds.`
+
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setShowMessage(true)
+      clearTimeout(timeoutId)
+    }, 8000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  if (authLoading) {
+    return (<div className="flex flex-col h-full w-full justify-center items-center">
+      <div className="text-6xl">Loading...</div>
+      <div className="text-xl">{showMessage ? message : ''}</div>
+    </div>)
+  }
 
   return (
     <div className="grid grid-cols-12 grid-rows-12 h-screen w-screen">

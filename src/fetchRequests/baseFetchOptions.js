5c4dd4e5ea7fc77849f5e19a515aa9e4fef2df1c
“Quinn <quinnlashinsky@gmail.com>
2020-10-05 13:28:31 -0400

    Delete fetchRequests folder and all native fetchRequests

--- src/fetchRequests/baseFetchOptions.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_PRODUCTION_API_URL
-    : process.env.REACT_APP_DEVELOPMENT_API_URL
-
-export const BASE_HEADERS = {
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
-}

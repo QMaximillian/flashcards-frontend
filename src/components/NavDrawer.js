e04651523ce3ac4a7dbba2f5e67ea6492b29dc30
“Quinn <quinnlashinsky@gmail.com>
2020-12-16 16:06:17 -0500

    Use one Route component to render null when accessing routes that will not show the NavDrawer component

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -1,14 +1,15 @@
-import React from 'react' // useEffect
+import React from 'react'
 import {Route, Switch} from 'react-router-dom'
-
 import MainNavDrawer from './MainNavDrawer'
 
 export default function NavDrawer() {
   return (
     <Switch>
-      <Route exact path="/card-sets/new" component={null}></Route>
-      <Route exact path="/card-sets/:id" component={null}></Route>
-      <Route path="/search" component={null}></Route>
+      <Route
+        exact
+        path={['/card-sets/new', '/card-sets/:id', '/search']}
+        component={null}
+      ></Route>
       <div
         className="row-start-1 row-end-13 col-start-1 col-end-4
           "

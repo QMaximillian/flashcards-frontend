868f18e957825c4b15d2e29e70aff0854935ff10
“Quinn <quinnlashinsky@gmail.com>
2020-12-16 01:06:58 -0500

    Remove component rendering of null in NavDrawer for redundant paths

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -3,25 +3,18 @@ import {Route, Switch} from 'react-router-dom'
 
 import MainNavDrawer from './MainNavDrawer'
 
-export default function NavDrawer(props) {
+export default function NavDrawer() {
   return (
     <Switch>
       <Route exact path="/card-sets/new" component={null}></Route>
       <Route exact path="/card-sets/:id" component={null}></Route>
-      <Route exact path="/card-sets/:id/edit" component={null}></Route>
-      <Route exact path="/search/:search" component={null}></Route>
-      <Route exact path="/search/" component={null}></Route>
-      <Route
-        path="/"
-        render={() => (
-          <div
-            className="row-start-1 row-end-13 col-start-1 col-end-4
+      <Route path="/search" component={null}></Route>
+      <div
+        className="row-start-1 row-end-13 col-start-1 col-end-4
           "
-          >
-            <MainNavDrawer />
-          </div>
-        )}
-      ></Route>
+      >
+        <Route path="/" component={MainNavDrawer}></Route>
+      </div>
     </Switch>
   )
 }

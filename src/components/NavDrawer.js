a8c45cc7ed68fd6b7e9029c3c8d7fbe73d142ec4
“Quinn <quinnlashinsky@gmail.com>
2020-12-16 22:20:45 -0500

    Add 'card-set/id/:edit' to NavDrawer route rendering a null component. Place div back in Route within Switch component to avoid computedmatch issue from React Router Switch component

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -7,12 +7,24 @@ export default function NavDrawer() {
     <Switch>
       <Route
         exact
-        path={['/card-sets/new', '/card-sets/:id', '/search', '/search/:id']}
+        path={[
+          '/card-sets/new',
+          '/card-sets/:id',
+          '/card-sets/:id/edit',
+          '/search',
+          '/search/:id',
+        ]}
         component={null}
       ></Route>
-      <div className="row-start-1 row-end-13 col-start-1 col-end-4 bg-gray-100 border-r-2 border-gray-200">
-        <Route path="/" component={MainNavDrawer}></Route>
-      </div>
+
+      <Route
+        path="/"
+        render={() => (
+          <div className="row-start-1 row-end-13 col-start-1 col-end-4 bg-gray-100 border-r-2 border-gray-200">
+            <MainNavDrawer />
+          </div>
+        )}
+      ></Route>
     </Switch>
   )
 }
